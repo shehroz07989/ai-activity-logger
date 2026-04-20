fe4727411a0bbec7bf256eadb8db9cdfddaf226c
Shehroz <shehrozearshad07@gmail.com>
2026-04-20 22:14:55 +0500

    pi

--- utils.py
+++ utils.py
@@ -1,37 +1,135 @@
-def status_check(response):
-    if 200 <= response.status_code < 300:
-        return response.json()
+def validate(data):
+    valid_users = []
+    invalid_users = []
+    invalid_data = []
+    if isinstance(data,dict):
+        results = data.get("results",[])
+        for index,i in enumerate(results,start=1):
+            if not isinstance(i,dict):
+                invalid_users.append({
+                     "valid":False,
+                     "reason": "not a dictionary",
+                     "user_no": index,
+                })
+                continue
+
+            name = i.get("name",{})
+            first_name = name.get("first","")
+            if not isinstance(name,dict):
+                invalid_users.append({
+                     "valid":False,
+                     "reason": "email missing",
+                     "user_no": index,
+                    })
+                continue
+            if not name_validator(first_name):
+                invalid_users.append({
+                     "valid":False,
+                     "reason": "name missing",
+                     "user_no": index ,
+                })
+                continue   
+            email = i.get("email", "")
+            if  not email_validator(email):
+                invalid_users.append({
+                     "valid":False,
+                     "reason": "email missing",
+                     "user_no": index,
+                })
+                continue
+            age = i.get("dob",{}).get("age",None)
+            if not age_validator(age):
+                invalid_users.append({
+                     "valid":False,
+                     "reason": "age missing",
+                     "user_no": index,
+                })
+                continue
+            valid_users.append({
+                 "valid": True,
+                 "name": first_name,
+                 "email": email,
+                 "age": age,
+            })
+        return {
+                "valid_users": valid_users,
+                "invalid_users": invalid_users,
+                }
     else:
-        return f"Status Code Error: {response.status_code}" 
-def clean_post(data):
-    cleaned_post = []
-    for i in data:
-        if i.get("id") and i.get("title"):
-            cleaned_post.append(
-                {
-                    "id": i.get("id"),
-                    "title": i.get("title"),
-                }
-            )
-    return cleaned_post
-
-def filter_post(cleaned_post):
-    filtered_post = []
-    for i in cleaned_post:
-        if i.get("id") > 5:
-            filtered_post.append(
-                {
-                    "id": i.get("id"),
-                    "title": i.get("title"),
-                }
-            )
-    return filtered_post
-
-def display_post(filtered_post):
-    display = []
-    for i in filtered_post:
-        display.append(
-            f"ID: {i.get('id')} - Title: {i.get('title')}",
+        invalid_data.append({
+                     "data":False,
+                     "reason": "data not a dictionary",
+                })
+        return invalid_data
         
-            )
-    return display
+                             
+    
+    
+
+
+def name_validator(first_name):   
+    if not isinstance(first_name,str):
+            return False
+    if first_name == "":
+                return False
+    return True
+            
+        
+def email_validator(email):
+        if not isinstance(email,str):
+            return False
+        if email.count("@") != 1:
+            return False
+        parts = email.split("@") 
+        left = parts[0]
+        right = parts[1]
+        if  left == "" or right == "":
+            return False
+        return True
+        
+def age_validator(age):
+        if not isinstance(age,int):
+            return False
+        if age <= 0:
+            return False
+        return True
+    
+def decision(data):
+    decision_users = []
+    valid = data.get("valid_users")
+    if valid:
+        for i in valid:
+            age = i.get("age",0)
+            name =  i.get("name","")
+            email =  i.get("email","")
+            if 17< age <= 25:
+                decision_users.append ({
+                   "rank": "junior",
+                  "name": name,
+                   "age": age,
+                   "email": email,
+                } )
+            elif 25 < age <= 40:
+                decision_users.append({
+               "rank": "senior",
+               "name": name,
+               "age": age,
+               "email": email,
+              })
+            elif age > 40:
+                decision_users.append({
+               "rank": "expert",
+               "name": name,
+               "age": age,
+               "email": email,
+                })
+            else:
+                decision_users.append({
+               "rank": "skip",
+               "name": name,
+               "age": age,
+               "email": email,
+                })
+        return {
+         "decision": decision_users
+        }
